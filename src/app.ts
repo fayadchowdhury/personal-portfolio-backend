fc6ca4559ad9eab75297d92ec6bfaa00283ff044
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-08-29 01:10:36 -0700

    Revert "Use /api to separate from frontend"
    This reverts commit 8b4c5fb10de1ea0885e1d9106b805d628941b717.

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const port: number = APP_CONFIG.port;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", routes);
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
