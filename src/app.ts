9e651755e52253579ad4aee3b7ad823b2adf5cf1
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-08-28 21:33:38 -0700

    Fix imports and add basic health check route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-const express = require('express');
-import type { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
+import { APP_CONFIG } from './config';
+import routes from './routes';
 
 const app: Application = express();
-const port: number = 3000;
+const port: number = APP_CONFIG.port;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!");
-});
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
