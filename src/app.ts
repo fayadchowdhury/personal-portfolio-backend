822a2a85412eb112ce8fff8a1c504d5605c299f4
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-08-28 17:55:58 -0700

    Create basic Express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log(`Hello World`);
+const express = require('express');
+import type { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello world!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
