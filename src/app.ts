8ee0c5de506d234f66d6de7cca197d49efa1b152
Ahmad Al Fayad Chowdhury <fayad.chowdhury@gmail.com>
2025-09-07 14:52:27 -0700

    Start Github sync cron job

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import { APP_CONFIG } from './config';
 import routes from './routes';
 import cors from 'cors';
 
+import { createGithubSyncJob } from "./jobs";
+
 const app: Application = express();
 const port: number = APP_CONFIG.port;
+
 
 app.use(cors());
 app.use(express.json());
@@ -12,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", routes);
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    const githubSyncJob = createGithubSyncJob();
+    githubSyncJob.start();
     console.log(`Server is running on port: ${port}`);
 });
